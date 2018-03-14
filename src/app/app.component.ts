dab8060e3607e00c9ea7bc0f8b694d058bbeca04
Kirill Avramenko <kavramenko2k@gmail.com>
2018-03-14 13:55:35 +0200

    qqq

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styles: [' h3 { color: dodgerblue; }']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  username = '';
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,12 +2,19 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: '<app-server></app-server><app-server></app-server>',
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
 
+  serversName = ['First Server', 'Second Server', 'Third Server'];
+  serverName = '';
+
   constructor() { }
+
+  addServer() {
+    this.serversName.push(this.serverName);
+  }
 
   ngOnInit() {
   }
